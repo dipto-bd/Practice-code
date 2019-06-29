40df940a5c855ff837e99c0d6be524830563f4ca
dipto-bd <dipto.bd.r@gmail.com>
2019-06-29 15:04:57 +0600

    first commit

---
+++ Two_pointer.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   int arr1[50].arr2[50];
+   int n,sum,min=0,a,b,x;
+   cin>>n;
+   for(int i=0;i<n;i++)
+   {
+      cin>>arr1[i]>>arr2[i];
+   }
+   cin>>x;
+
+   return 0;
+}
